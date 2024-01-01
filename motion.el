136d7caae075f7b12fc686c90471f392a69670d8
Leo Gaskin <leo.gaskin@le0.gs>
2024-01-01 20:47:16 +0100

    Fix minimum supported Emacs version

--- motion.el
+++ motion.el
@@ -7,7 +7,7 @@
 ;; Homepage: https://github.com/leotaku/motion
 ;; Keywords: emulations
 ;; Package-Version: 0.1.0
-;; Package-Requires: ((emacs "29.0"))
+;; Package-Requires: ((emacs "29.1"))
 
 ;; This program is free software: you can redistribute it and/or modify
 ;; it under the terms of the GNU General Public License as published by
